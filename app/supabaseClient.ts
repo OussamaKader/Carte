2fdd91061f597a09316592ce6fde32bf1599381d
Oussama Med Lemine <oussamamedlemine167@gmail.com>
2026-04-27 23:31:21 +0100

    add full project

--- app/supabaseClient.ts
+++ app/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://mnevjsmexwgotqzecfcb.supabase.co';
-const supabaseKey = 'sb_publishable_WpIlZdCOwCAbzR7bOKeNHA_dAuzPNzY';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
